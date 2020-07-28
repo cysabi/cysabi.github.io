ca6e1f35d6bd075c3d2c63b6014c5a85b2159fb0
LeptoFlare <leptoflare@gmail.com>
2020-07-28 18:48:37 -0400

    Create twitch/faq page

---
+++ src/pages/twitch/faq.js
@@ -0,0 +1,23 @@
+import React from "react"
+
+import Layout from "src/components/Layout"
+import Heading from "src/components/Heading"
+
+const Index = () => (
+  <Layout pageTitle="Hey, I'm LeptoFlare">
+    <div class="section container">
+      <Heading>faq</Heading>
+      <h6>who made the panels?</h6>
+      <p>me :o</p>
+      <p>
+        the faq
+        <br />
+        is under construction!
+      </p>
+      <h6>When will the FAQ be finished?</h6>
+      <p>errrmm monkaS</p>
+    </div>
+  </Layout>
+)
+
+export default Index
