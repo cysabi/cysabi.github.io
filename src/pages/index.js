47d9ed5c53ed568ebe3efe1761b586dafc904fdf
LeptoFlare <leptoflare@gmail.com>
2020-07-31 01:35:09 -0400

    Create HeaderButton component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 
 import Layout from "src/components/Layout"
+import HeaderButton from "src/components/HeaderButton"
 
 const Index = () => (
   <Layout pageTitle="Hey, I'm LeptoFlare">
@@ -15,155 +16,108 @@ const Index = () => (
         </p>
       </div>
       <div class="hero-foot mb-1 columns is-centered has-text-centered">
-        <div class="column is-hidden-touch">
+        <div class="column">
           <h4>Links</h4>
           <div class="field is-grouped is-grouped-centered has-addons">
-            <a href="/blog" class="button is-medium is-text">
-              <span class="icon">
-                <i class="fas fa-blog" />
-              </span>
-              <span>Blog</span>
-            </a>
-            <a href="/github/pages" class="button is-medium is-text">
-              <span class="icon">
-                <i class="fab fa-github-square"></i>
-              </span>
-              <span>Github Pages</span>
-            </a>
+            <HeaderButton href="/blog" fa="fas fa-blog">
+              Blog
+            </HeaderButton>
+            <HeaderButton href="/github/pages" fa="fab fa-github-square">
+              Github Pages
+            </HeaderButton>
           </div>
         </div>
-        <div class="column is-narrow is-hidden-touch">
+        <div class="column is-narrow">
           <h4>Contact</h4>
           <div class="field is-grouped is-grouped-centered has-addons">
-            <a href="/discord" class="button is-medium is-text">
-              <span class="icon">
-                <i class="fab fa-discord" />
-              </span>
-              <span>Discord</span>
-            </a>
-            <a href="/github" class="button is-medium is-text">
-              <span class="icon">
-                <i class="fab fa-github" />
-              </span>
-              <span>Github</span>
-            </a>
-            <a href="/twitch" class="button is-medium is-text">
-              <span class="icon">
-                <i class="fab fa-twitch" />
-              </span>
-              <span>Twitch</span>
-            </a>
-            <a
-              href="mailto:leptoflare@lepto.tech"
-              class="button is-medium is-text"
-            >
-              <span class="icon">
-                <i class="fas fa-envelope" />
-              </span>
-              <span>Email</span>
-            </a>
+            <HeaderButton href="/discord" fa="fab fa-discord">
+              Discord
+            </HeaderButton>
+            <HeaderButton href="/github" fa="fab fa-github">
+              Github
+            </HeaderButton>
+            <HeaderButton href="/twitch" fa="fab fa-twitch">
+              Twitch
+            </HeaderButton>
+            <HeaderButton href="/email" fa="fas fa-envelope">
+              Email
+            </HeaderButton>
           </div>
         </div>
-        <div class="column is-hidden-touch">
+        <div class="column">
           <h4>Support me!</h4>
           <div class="field is-grouped is-grouped-centered has-addons">
-            <a
-              class="button is-medium is-text px-0 mx-1"
-              href="https://www.patreon.com/join/leptoflare"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <img
-                height="36"
-                style={{
-                  borderRadius: "8px",
-                  border: "0px",
-                  height: "36px",
-                  maxWidth: "none",
-                }}
-                src="https://c5.patreon.com/external/logo/become_a_patron_button.png"
-                border="0"
-                alt="Become a Patron at patreon.com"
-              />
-            </a>
-            <a
-              class="button is-medium is-text px-0 mx-1"
-              href="https://ko-fi.com/leptoflare"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <img
-                height="36"
-                style={{ border: "0px", height: "36px", maxWidth: "none" }}
-                src="https://cdn.ko-fi.com/cdn/kofi3.png?v=2"
-                border="0"
-                alt="Buy Me a Coffee at ko-fi.com"
-              />
-            </a>
-          </div>
-        </div>
-
-        <div class="column is-hidden-desktop">
-          <h4>Pages</h4>
-          <div class="field is-grouped is-grouped-centered has-addons">
-            <a href="/blog" class="button is-medium is-text">
-              <span class="icon">
-                <i class="fas fa-blog" />
-              </span>
-            </a>
-            <a href="/github/pages" class="button is-medium is-text">
-              <span class="icon">
-                <i class="fab fa-github-square"></i>
-              </span>
-            </a>
-          </div>
-        </div>
-        <div class="column is-narrow is-hidden-desktop">
-          <h4>Contact</h4>
-          <div class="field is-grouped is-grouped-centered has-addons">
-            <a href="/discord" class="button is-medium is-text">
-              <span class="icon">
-                <i class="fab fa-discord" />
-              </span>
-            </a>
-            <a href="/github" class="button is-medium is-text">
-              <span class="icon">
-                <i class="fab fa-github" />
-              </span>
-            </a>
-            <a href="/twitch" class="button is-medium is-text">
-              <span class="icon">
-                <i class="fab fa-twitch" />
-              </span>
-            </a>
-            <a
-              href="mailto:leptoflare@lepto.tech"
-              class="button is-medium is-text"
-            >
-              <span class="icon">
-                <i class="fas fa-envelope" />
-              </span>
-            </a>
-          </div>
-        </div>
-        <div class="column is-hidden-desktop">
-          <h4>Support me!</h4>
-          <div class="field is-grouped is-grouped-centered has-addons">
-            <a class="button is-medium is-text" href="https://www.patreon.com/join/leptoflare">
-              <span class="icon">
-                <i class="fab fa-patreon" />
-              </span>
-            </a>
-            <a class="button is-medium is-text" href="https://ko-fi.com/leptoflare">
-              <span class="icon">
-                <i class="fas fa-coffee" />
-              </span>
-            </a>
+            <SupportKofi />
+            <SupportPatreon />
           </div>
         </div>
       </div>
     </div>
   </Layout>
+)
+
+const SupportPatreon = () => (
+  <>
+    <div class="is-hidden-touch">
+      <a
+        class="button is-medium is-text px-0 mx-1"
+        href="https://www.patreon.com/join/leptoflare"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img
+          height="36"
+          style={{
+            borderRadius: "8px",
+            border: "0px",
+            height: "36px",
+            maxWidth: "none",
+          }}
+          src="https://c5.patreon.com/external/logo/become_a_patron_button@2x.png"
+          border="0"
+          alt="Become a Patron at patreon.com"
+        />
+      </a>
+    </div>
+    <div class="is-hidden-desktop">
+      <a
+        class="button is-medium is-text"
+        href="https://www.patreon.com/join/leptoflare"
+      >
+        <span class="icon">
+          <i class="fab fa-patreon" />
+        </span>
+      </a>
+    </div>
+  </>
+)
+
+const SupportKofi = () => (
+  <>
+    <div class="is-hidden-touch">
+      <a
+        class="button is-medium is-text px-0 mx-1"
+        href="https://ko-fi.com/leptoflare"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img
+          height="36"
+          style={{ border: "0px", height: "36px", maxWidth: "none" }}
+          src="https://cdn.ko-fi.com/cdn/kofi3.png?v=2"
+          border="0"
+          alt="Buy Me a Coffee at ko-fi.com"
+        />
+      </a>
+    </div>
+    <div class="is-hidden-desktop">
+      <a class="button is-medium is-text" href="https://ko-fi.com/leptoflare">
+        <span class="icon">
+          <i class="fas fa-coffee" />
+        </span>
+      </a>
+    </div>
+  </>
 )
 
 export default Index
