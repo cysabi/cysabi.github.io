dd8cae2805d2ee8b269c52d76db615db2866a6e3
LeptoFlare <47790183+LeptoFlare@users.noreply.github.com>
2022-10-24 05:29:09 -0400

    lets see if this works - solidjs

--- gatsby-config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  siteMetadata: {
-    title: "lepto.tech",
-    siteUrl: "https://lepto.tech",
-  },
-  plugins: ["gatsby-plugin-postcss"],
-}
--- src/pages/discord.js
+++
@@ -1,7 +0,0 @@
-import React from "react"
-
-import Redirect from "../components/Redirect"
-
-const Discord = () => <Redirect to="https://discord.gg/faUNrfP" />
-
-export default Discord
