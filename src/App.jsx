4917a72b6e6749b51979b3621e72a91cda978996
LeptoFlare <47790183+LeptoFlare@users.noreply.github.com>
2023-08-05 17:49:17 -0400

    fonts work + start dev work

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
+import grid from "./static/grid.svg"
+
 const Index = () => (
-  <main>
-    <div class="text-4xl">hey, i'm cerusabi</div>
-    <div class="text-4xl">born to design, forced to develop</div>
+  <main class="relative">
+    <div class="absolute z-10">
+      <div class="py-24 max-w-5xl mx-auto">
+        <div class="text-4xl">hey, i'm cerusabi</div>
+        <div class="text-4xl">born to design, forced to develop</div>
+      </div>
+    </div>
+    <div class="absolute z-0">
+      <div>
+        <img
+          src={grid}
+          style="mask-image: linear-gradient(#000, transparent);"
+        />
+      </div>
+    </div>
   </main>
 )
 
