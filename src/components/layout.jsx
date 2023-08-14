c00287611824e3d454b64d27854982f61f5ebf41
LeptoFlare <47790183+LeptoFlare@users.noreply.github.com>
2023-08-14 01:49:31 -0400

    seperate root & layout

---
+++ src/components/layout.jsx
@@ -0,0 +1,82 @@
+import { onMount } from "solid-js"
+import { A, Outlet } from "@solidjs/router"
+import Grid, { useGrid } from "./grid"
+
+const Layout = props => {
+  const { setPos, setColor } = useGrid()
+  onMount(() => {
+    setColor(false)
+  })
+  return (
+    <>
+      {props.main === true || (
+        <div class="flex items-center font-medium p-8 gap-12">
+          <A
+            href="/"
+            class="text-2xl underline decoration-2 decoration-transparent hover:decoration-slate-400 transition-colors"
+          >
+            cerusabi
+          </A>
+          <div class="text-xl flex items-center gap-8">
+            <A
+              href="/#about"
+              class="underline decoration-2 decoration-transparent hover:decoration-slate-400 transition-colors"
+            >
+              about
+            </A>
+            <A
+              href="/works"
+              class="underline decoration-2 decoration-transparent hover:decoration-slate-400 transition-colors"
+            >
+              works
+            </A>
+          </div>
+        </div>
+      )}
+      <main
+        onMouseMove={e => setPos({ x: e.clientX, y: e.clientY })}
+        class="relative min-h-screen overflow-hidden"
+      >
+        <Outlet />
+        {props.children}
+        <div class="bg-gray-800/50 border-t-4 border-gray-700/25 backdrop-blur-3xl flex flex-col gap-8 p-12">
+          <div class="flex items-center gap-4 w-full">
+            <div class="text-slate-500 text-center">
+              built with{" "}
+              <a
+                href="https://solidjs.com"
+                class="text-slate-400 hover:underline"
+              >
+                solidjs
+              </a>{" "}
+              (
+              <a
+                href="https://vitejs.dev"
+                class="text-slate-400 hover:underline"
+              >
+                vite
+              </a>
+              ) &{" "}
+              <a
+                href="https://tailwindcss.com"
+                class="text-slate-400 hover:underline"
+              >
+                tailwind
+              </a>
+              ,{" "}
+              <a
+                href="https://github.com/LeptoFlare/LeptoFlare.github.io"
+                class="text-slate-400 hover:underline"
+              >
+                source code
+              </a>
+            </div>
+          </div>
+        </div>
+      </main>
+      <Grid />
+    </>
+  )
+}
+
+export default Layout
--- src/root.jsx
+++ src/root.jsx
@@ -1,87 +1,12 @@
 import { render } from "solid-js/web"
-import { A, Outlet, Route, Router, Routes } from "@solidjs/router"
-import Grid, { GridProvider, useGrid } from "./components/grid"
+import { Route, Router, Routes } from "@solidjs/router"
 import Index from "./routes/index"
 import Works, { works } from "./routes/works/index"
 import WorkTemplate from "./routes/works/_template"
-import "./root.css"
-import { onMount } from "solid-js"
+import { GridProvider } from "./components/grid"
+import Layout from "./components/layout"
 import Img from "./components/img"
-
-const Root = props => {
-  const { setPos, setColor } = useGrid()
-  onMount(() => {
-    setColor(false)
-  })
-  return (
-    <>
-      {props.main === true || (
-        <div class="flex items-center font-medium p-8 gap-12">
-          <A
-            href="/"
-            class="text-2xl underline decoration-2 decoration-transparent hover:decoration-slate-400 transition-colors"
-          >
-            cerusabi
-          </A>
-          <div class="text-xl flex items-center gap-8">
-            <A
-              href="/#about"
-              class="underline decoration-2 decoration-transparent hover:decoration-slate-400 transition-colors"
-            >
-              about
-            </A>
-            <A
-              href="/works"
-              class="underline decoration-2 decoration-transparent hover:decoration-slate-400 transition-colors"
-            >
-              works
-            </A>
-          </div>
-        </div>
-      )}
-      <main
-        onMouseMove={e => setPos({ x: e.clientX, y: e.clientY })}
-        class="relative min-h-screen overflow-hidden"
-      >
-        <Outlet />
-        {props.children}
-        <div class="flex flex-col gap-8 max-w-7xl mx-auto p-[min(10vw,128px)]">
-          <div class="flex items-center gap-4 w-full">
-            <div class="flex-1 h-0.5 bg-slate-50 rounded-full" />
-          </div>
-          <div class="text-slate-400 text-center">
-            built with{" "}
-            <a
-              href="https://solidjs.com"
-              class="text-slate-200 hover:underline"
-            >
-              solidjs
-            </a>{" "}
-            (
-            <a href="https://vitejs.dev" class="text-slate-200 hover:underline">
-              vite
-            </a>
-            ) &{" "}
-            <a
-              href="https://tailwindcss.com"
-              class="text-slate-200 hover:underline"
-            >
-              tailwind
-            </a>
-            ,{" "}
-            <a
-              href="https://github.com/LeptoFlare/LeptoFlare.github.io"
-              class="text-slate-200 hover:underline"
-            >
-              source code
-            </a>
-          </div>
-        </div>
-      </main>
-      <Grid />
-    </>
-  )
-}
+import "./root.css"
 
 render(
   () => (
@@ -91,12 +16,12 @@ render(
           <Route
             path="/"
             element={
-              <Root main={true}>
+              <Layout main={true}>
                 <Index />
-              </Root>
+              </Layout>
             }
           />
-          <Route path="/" component={Root}>
+          <Route path="/" component={Layout}>
             <Route path="/works" component={Works} />
             {works.map(Work => (
               <Route
