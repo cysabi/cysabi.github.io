fa0fee3fa181a8697719b75ab7308ff133486b45
LeptoFlare <47790183+LeptoFlare@users.noreply.github.com>
2024-04-06 01:05:09 -0400

    new

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -23,68 +23,8 @@ const Layout = () => {
           setPointer(getComputedStyle(e.target).cursor)
           setPos({ x: e.clientX, y: e.clientY })
         }}
-        class="relative min-h-screen overflow-clip flex flex-col"
       >
-        {location.pathname === "/" || (
-          <div class="flex items-center flex-wrap font-medium p-8 gap-12">
-            <A href="/" class="text-2xl">
-              cysabi
-            </A>
-            <div class="text-xl flex items-center gap-8">
-              <A href="/#works">works</A>
-              <A href="/#contact">contact</A>
-            </div>
-          </div>
-        )}
-        <div class="flex-1">
-          <Outlet />
-        </div>
-        <div class="bg-slate-900/50 border-t-4 border-slate-700/25 backdrop-blur flex flex-col gap-8 mt-[min(10vw,128px)] p-12">
-          <div class="flex items-center justify-between flex-wrap gap-12 w-full">
-            <div class="text-slate-500 text-center">
-              like what you see?{" "}
-              <A
-                href="/#contact"
-                onClick={() => window.scrollTo(0, 0)}
-                class="text-slate-400 decoration-transparent"
-              >
-                contact me!
-              </A>
-            </div>
-            <div class="text-slate-500">
-              built with{" "}
-              <a
-                href="https://solidjs.com"
-                class="text-slate-400 decoration-transparent"
-              >
-                solidjs
-              </a>{" "}
-              (
-              <a
-                href="https://vitejs.dev"
-                class="text-slate-400 decoration-transparent"
-              >
-                vite
-              </a>
-              ) &{" "}
-              <a
-                href="https://tailwindcss.com"
-                class="text-slate-400 decoration-transparent"
-              >
-                tailwind
-              </a>
-              <span class="px-3 font-mono font-black text-slate-600 -tracking-widest">
-                //
-              </span>
-              <a
-                href="https://github.com/LeptoFlare/LeptoFlare.github.io"
-                class="text-slate-400 decoration-transparent"
-              >
-                source code
-              </a>
-            </div>
-          </div>
-        </div>
+        <Outlet />
       </main>
       <Grid />
     </>
